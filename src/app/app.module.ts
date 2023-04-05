d5b40622b892eb45502c9b70711ea5868b40a180
Guillo7799 <guillermo.rivera@epn.edu.ec>
2023-04-05 14:26:00 -0500

    Proyecto DBZ finalziado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
+import { MainPageComponent } from './dbz/main-page/main-page.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeroesModule, ContadorModule],
+  imports: [BrowserModule, HeroesModule, ContadorModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
